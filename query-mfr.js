8ff60259e837ced33aa1f352c47a8bb15303b7d5
ameba23 <ameba23@systemli.org>
2019-07-03 16:46:04 +0200

    changed to use kappa-view-query

--- query-mfr.js
+++ query-mfr.js
@@ -1,28 +1,42 @@
 const kappa = require('kappa-core')
 const pull = require('pull-stream')
-var core = kappa('./multimetadb', {valueEncoding: 'json'})
 const level = require('level')
-const View = require('./view-mfr')
-
-const idx = level('./views')
-const view = View(idx)
+const Query = require('kappa-view-query')
 const pump = require('pump')
-
 const discovery = require('discovery-swarm')
+
+var core = kappa('./multimetadb', {valueEncoding: 'json'})
+const db = level('./views')
+
+// custom validator enabling you to write your own message schemas
+const validator = function (msg) {
+  if (typeof msg !== 'object') return null
+  if (typeof msg.value !== 'object') return null
+  if (typeof msg.value.id !== 'string') return null
+  // if (typeof msg.value.type !== 'string') return null
+  return msg
+}
+
+const indexes = [
+  // indexes all messages from all feeds by timestamp 
+  { key: 'log', value: ['value', 'timestamp'] },
+  // indexes all messages from all feeds by message type, then by timestamp 
+  { key: 'typ', value: [['value', 'type'], ['value', 'timestamp']] }
+] 
+
+core.use('query', Query(db, core, { indexes, validator })) 
 
 var swarm = discovery()
 
 swarm.join('mouse-p2p-app')
 
-core.use('query', view)
-
 core.ready(() => {
-  const $filter = { value: { type: 'addFile', metadata: { track: 3 } } }
+  const query = [{ $filter: { value: { type: 'addFile', metadata: { track: 3 } } } }]
   // var feeds = core.feeds()
   // console.log('feeds', feeds)
   // const $filter = {}
   pull(
-    core.api.query.read({ query: [{ $filter }] }),
+    core.api.query.read({ live: true, reverse: true, query }),
     pull.map(entry => {
       return entry.value
     }),
