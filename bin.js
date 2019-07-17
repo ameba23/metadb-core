2b8d13e4b74dcbbe7f4bff7a8513a671d190d97e
ameba23 <ameba23@systemli.org>
2019-07-17 10:08:12 +0200

    add yargs command line stuff

---
+++ bin.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+const yargs = require('yargs')
+
+const metadb = require('.')
+
+const yargsargs = processCommand()
+if (!yargsargs._[0]) yargs.showHelp()
+
+function callback (err, res) {
+  if (err) throw err
+  console.log(res)
+}
+
+function processCommand () {
+  return yargs
+    .command('index <directory>', 'index a directory', (yargs) => {
+      yargs
+        .positional('directory', {
+          describe: 'directory to scan',
+          type: 'string'
+        })
+        .option('feedname', {
+          demandOption: false,
+          type: 'string'
+        })
+    }, (argv) => {
+      metadb.indexFiles(argv.directory, argv.feedname)
+    })
+
+    .command('name <name>', 'give yourself a name', (yargs) => {
+      yargs
+        .positional('name', {
+          describe: 'a name',
+          type: 'string'
+        })
+        .option('feedname', {
+          demandOption: false,
+          type: 'string'
+        })
+    }, (argv) => {
+      metadb.publishAbout(argv.name, argv.feedname, callback)
+    })
+    .argv
+}
