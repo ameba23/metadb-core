c27e3741293c738e57e793f0220242ff6c0d92a4
ameba23 <ameba23@systemli.org>
2019-07-18 22:02:04 +0200

    add swarm.js

---
+++ swarm.js
@@ -0,0 +1,22 @@
+const discovery = require('discovery-swarm')
+const pump = require('pump')
+
+module.exports = function (core) {
+  return function swarm (key) {
+    // TODO: use dat-swarm-defaults?
+    // add id property with local key?  (cabal does this)
+    var swarm = discovery()
+
+    // TODO: change this
+    key = key || 'mouse-p2p-app'
+    swarm.join(key)
+
+    swarm.on('connection', (connection, peer) => {
+      console.log('New peer connected with key ', peer.id.toString('hex'))
+      // TODO: pump can also take a callback?
+      pump(connection, core.replicate({ live: true }), connection)
+    })
+
+    // callback(null, swarm) ?
+  }
+}
