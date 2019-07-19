5751326622ebb36f189f10731f53e77cbebde2ad
ameba23 <ameba23@systemli.org>
2019-07-19 08:13:57 +0200

    mkdirp db directory

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const kappa = require('kappa-core')
 const path = require('path')
 const os = require('os')
+const mkdirp = require('mkdirp')
 
 const IndexFiles = require('./index-kappacore')
 const QueryMfr = require('./query-mfr')
@@ -9,7 +10,7 @@ const Swarm = require('./swarm')
 
 const METADB_PATH = './metadb'
 // const METADB_PATH = path.join(os.homedir(), '.metadb')
-
+mkdirp.sync(METADB_PATH)
 const DB_PATH = path.join(METADB_PATH + '/db')
 
 var core = kappa(DB_PATH, { valueEncoding: 'json' })
