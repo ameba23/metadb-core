43fe59620de21e90f6b4819b0ece3d1607c7481b
ameba23 <ameba23@systemli.org>
2019-08-21 17:25:20 +0200

    update request test

--- test/publish-request.test.js
+++ test/publish-request.test.js
@@ -1,25 +1,28 @@
 const test = require('tape')
-const metadb = require('..')
+const MetaDb = require('..')
 const pull = require('pull-stream')
+const tmpDir = require('tmp').dirSync
 
-const files = [ 'xvhiEpLSt/XFGCcHmim/4/r6i0InGaJ6GNPS19ciolY=.sha256' ]
+const files = ['xvhiEpLSt/XFGCcHmim/4/r6i0InGaJ6GNPS19ciolY=.sha256']
 const recipients = []
 
 test('publish a request message', t => {
-  metadb.publishRequest(files, recipients, null, (err, seq) => {
-    t.notOk(err, 'does not throw err')
-    metadb.queryMfr(() => {
-      pull(
-        metadb.query([{ $filter: { value: { type: 'request' } } }]),
-        // metadb.query(),
-        pull.collect((err, requests) => {
-          // todo: isabout()
-
-          console.log(requests)
-          t.ok(requests.length > 0, 'the request message exists')
-          t.end()
-        })
-      )
+  var metaDb = MetaDb({ path: tmpDir().name })
+  metaDb.ready(() => {
+    metaDb.publishRequest(files, recipients, (err, seq) => {
+      t.notOk(err, 'does not throw err')
+    console.log(metaDb.localFeed.key.toString('hex'))
+      metaDb.buildIndexes(() => {
+        pull(
+          metaDb.query([{ $filter: { value: { type: 'request', files } } }]),
+          pull.collect((err, requests) => {
+            // todo: isabout()
+            t.error(err, 'does not throw err')
+            t.equal(requests.length, 1, 'The request message exists')
+            t.end()
+          })
+        )
+      })
     })
   })
 })
