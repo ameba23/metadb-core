74d9e2713c5d232a32193f0f03a51a5c7412b443
ameba23 <ameba23@systemli.org>
2019-09-15 08:48:33 +0200

    hashes to files

---
+++ queries/own-files-from-hashes.js
@@ -0,0 +1,25 @@
+// const merge = require('deepmerge')
+const pull = require('pull-stream')
+const path = require('path')
+
+// for my files only, given an array of hashes, return an array of filenames
+
+module.exports = function (metaDb) {
+  return function (hashList, callback) { // opts?
+    const key = metaDb.key.toString('hex')
+    pull(
+      metaDb.query([{ $filter: { key, value: { type: 'addFile' } } }]),
+      pull.filter(file => hashList.indexOf(file.value.id) > -1),
+      metaDb.map(file => getFullPath(file.value.filename, file.seq, metaDb.shares)),
+      pull.collect(callback)
+    )
+  }
+}
+
+function getFullPath (filename, seq, shares) {
+  const sharePath = shares[Object.keys(shares).find(s => s >= seq)]
+  return path.join(sharePath, filename)
+}
+
+// example:  highest sequence number added
+//   { 5: '~/music', 50: '/docs', 123: '~/speedcore' }
