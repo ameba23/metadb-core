4f869110296d2bc10b45eab7f1d6047e2232ba15
ameba23 <ameba23@systemli.org>
2020-03-03 19:57:14 +0100

    handle filenames

--- queries/processRequestsFromSelf.js
+++ queries/processRequestsFromSelf.js
@@ -1,5 +1,7 @@
 const pull = require('pull-stream')
+const path = require('path')
 const { download } = require('../transfer/hypercore-sendfile') // publishFiles
+const log = console.log // debug
 
 module.exports = function (metadb) {
   return function (callback) {
@@ -14,10 +16,29 @@ module.exports = function (metadb) {
           pull.asyncMap((reply, cb2) => {
             if (!request.closed) {
               // hash to filename / folder
-              download(reply.link, metadb.downloadPath, (err, network) => {
-                if (err) return cb2(err)
-                // metadb.pendingDownloads.push(network) // TODO somehow check its not already there
-                cb2(null, network)
+              // just take the first file for now, but eventually this will be
+              // an asyncmap
+              metadb.files.get(request.files[0], (err, requestedFileMetadata) => {
+                if (err) {
+                  return cb2(err)
+                  // we should allow requesting for files we dont know about
+                  // filename = request.files[0] // name it with the hash?
+                }
+                const filePath = path.join(metadb.downloadPath, requestedFileMetadata.filename)
+                download(reply.link, filePath, onDownloaded, (err, network) => {
+                  if (err) return cb2(err)
+                  // metadb.pendingDownloads.push(network) // TODO somehow check its not already there
+                  cb2(null, network)
+                })
+
+                function onDownloaded (hashToCheck) {
+                  if (hashToCheck.toString('hex') === request.files[0]) {
+                    log('downloaded file verified')
+                  } else {
+                    log('downloaded file hash does not match requested!')
+                  }
+                  metadb.requests.update(request.msgSeq, { closed: true })
+                }
               })
             }
           }),
--- transfer/hypercore-sendfile.js
+++ transfer/hypercore-sendfile.js
@@ -2,6 +2,7 @@
 const replicator = require('@hyperswarm/replicator')
 const hypercore = require('hypercore')
 const ram = require('random-access-memory')
+const sodium = require('sodium-native')
 // const raf = require('random-access-file')
 // const tar = require('tar-fs')
 const fs = require('fs')
@@ -15,7 +16,7 @@ let activeUploads = []
 module.exports = { publish, upload, download }
 
 function publish (fileObjects, baseDir, callback) {
-  console.log('published called', fileObjects)
+  log('published called', fileObjects)
 
   if (activeUploads.includes(fileObjects[0].hash)) return callback(null, false)
   activeUploads.push(fileObjects[0].hash) // TODO this should be a db write
@@ -24,12 +25,19 @@ function publish (fileObjects, baseDir, callback) {
 
 function upload (fileObject, callback) {
   const { file, hash } = fileObject
-  const keypair = crypto.keypair(Buffer.from(hash, 'hex'))
-  const options = { key: keypair.publicKey, secretKey: keypair.secretKey }
+
+  // Derive a keypair from the hash of the file
+  //  - good because it gives content-addressing - other peers can join
+  //  - bad for security - so currently not using
+  // const keypair = crypto.keypair(Buffer.from(hash, 'hex'))
+  // const optionsForHypercore = { key: keypair.publicKey, secretKey: keypair.secretKey }
+
   // const feed = hypercoreIndexedFile(file, options, err => onfeed(err))
 
-  const feed = hypercore(ram, options)
+  const feed = hypercore(createStorage) // ram
+
   // tar.pack(baseDir, { entries: files}).pipe(feed.createWriteStream())
+
   fs.createReadStream(file).pipe(feed.createWriteStream())
   onfeed()
 
@@ -47,20 +55,24 @@ function upload (fileObject, callback) {
       log('[publish] peer removed')
     })
     feed.on('sync', () => {
-      log('[publish] sync called!!!!')
+      log('[publish] sync called!!!!') // TODO i dont think this ever gets called
     })
     // TODO add a prefix.
     callback(null, feed.key.toString('hex'), swarm)
   }
 }
 
-function download (link, downloadPath, callback) {
+function download (link, downloadPath, onDownloaded, callback) {
   if (activeDownloads.includes(link)) return callback(null, false)
   activeDownloads.push(link)
+
   const key = Buffer.from(link, 'hex') // TODO validation/processing
+
   console.log('******download called*******', link)
-  // if (link.slice(0, 6) === 'dat://') link = link.slice(6) // TODO get rid 
+  // if (link.slice(0, 6) === 'dat://') link = link.slice(6) // TODO get rid
   if (key.length !== 32) return callback(new Error('link is wrong length'))
+
+  const hashToCheckInstance = sodium.crypto_hash_sha256_instance()
   const feed = hypercore(ram, key)
   const swarm = replicator(feed)
   swarm.on('connection', (socket, details) => {
@@ -78,27 +90,42 @@ function download (link, downloadPath, callback) {
   feed.on('close', peer => {
     log('[download] feed closed')
   })
-  // TODO filename
-  const target = fs.createWriteStream(path.join(downloadPath, link))
+  // TODO multiple filenames
+  const target = fs.createWriteStream(downloadPath)
   // const target = tar.extract(downloadPath)
 
   feed.createReadStream({ live: true }).pipe(target)
+
+  feed.on('data', (chunk) => {
+    console.log('[download] chunk added') // temporary
+    // TODO should be able to verifiy parts of big files.
+    hashToCheckInstance.update(chunk)
+    // could also stream to the front end here
+  })
+
   feed.on('sync', () => {
-    log('File downlowded')
+    log('[download] File downlowded')
     swarm.leave(feed.discoveryKey)
     activeDownloads = activeDownloads.filter(i => i !== link)
-    // TODO stop this being called again
+    feed.close() // Not sure if this is needed
+
+    const hashToCheck = sodium.sodium_malloc(sodium.crypto_hash_sha256_BYTES)
+    hashToCheckInstance.final(hashToCheck)
+    onDownloaded(hashToCheck)
   })
+
   callback(null, swarm)
 }
 
 // adapted from hypercore-indexed-file
-// function createStorage (file) {
-//   return (filename) => {
-//     if (filename.endsWith('data')) {
-//       return raf(pathspec)
-//     } else {
-//       return ram(filename)
-//     }
-//   }
-// }
+function createStorage (file) {
+  return (filename) => {
+    log(`[storage] ${filename}`)
+    return ram(filename)
+  }
+  // if (filename.endsWith('data')) {
+  //   return raf(pathspec)
+  // } else {
+  //   return ram(filename)
+  // }
+}
