5b1963014524e300d88dda3a7e451e31bcd3d512
ameba23 <ameba23@systemli.org>
2020-04-30 16:12:28 +0200

    add test for swarm.js

---
+++ test/swarm.test.js
@@ -0,0 +1,44 @@
+const test = require('tape')
+const Metadb = require('..')
+const tmpDir = require('tmp').dirSync
+const async = require('async')
+const path = require('path')
+const names = ['alice', 'bob']
+
+test('request and reply, 2 actors', t => {
+  const metadbs = []
+  async.each(names, (name, callback) => {
+    const metadb = Metadb({ storage: tmpDir().name, test: true })
+    metadb.ready(() => {
+      metadb.publish.about(name, (err, seq) => {
+        t.error(err, 'does not throw err')
+        metadb.buildIndexes(() => {
+          metadb.swarm('testswarm', (err, swarms) => {
+            t.error(err, 'no error on connect to swarm')
+            t.equals(swarms.length, 1, 'correct number of swarms')
+            t.equals(swarms[0], 'testswarm', 'correct swarm')
+            console.log('feeds', metadb.core.feeds().length)
+            metadbs.push(metadb)
+            callback()
+          })
+        })
+      })
+    })
+  }, (err) => {
+    t.error(err, 'No error')
+    // wait till we hear about another feed
+    metadbs[0].core._logs.on('feed', () => {
+      t.equals(metadbs[0].core.feeds().length, 2, 'we now have two feeds')
+      console.log('feeds', metadbs[1].core.feeds().length)
+      metadbs[0].unswarm('testswarm', (err, swarms) => {
+        t.error(err, 'No error on first instance disconnecting')
+        t.equals(swarms.length, 0, 'no connected swarms')
+        metadbs[1].unswarm('testswarm', (err, swarms) => {
+          t.error(err, 'No error on second instance disconnecting')
+          t.equals(swarms.length, 0, 'no connected swarms')
+          t.end()
+        })
+      })
+    })
+  })
+})
